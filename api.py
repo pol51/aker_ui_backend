92b8f34253a04eb31354151322bc14914df26656
Paul Labedan <plabedan@gmail.com>
2018-04-21 00:17:40 +0200

    fix CORS

--- api.py
+++ api.py
@@ -6,7 +6,15 @@ from models import *
 db = DB()
 Base.metadata.bind = db.engine
 
+
+def add_cors_header(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Access-Control-Allow-Origin, Content-Type, Accept'
+    return response
+
+
 app = Flask(__name__)
+app.after_request(add_cors_header)
 apimanager = APIManager(app, session=db.connetion)
 user_api_blueprint = apimanager.create_api_blueprint(User, methods=['GET'])
 host_api_blueprint = apimanager.create_api_blueprint(Host, methods=['GET'])
